09843b98e82419dddb20c52ed96110de678b7d21
murshidameen <murshidmonzpv@gmail.com>
2023-11-16 17:44:13 +0530

    setting up express server

---
+++ server/src/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.send("App")
+})
+
+app.listen(port, () => {
+    console.log(`Server listening on port http://localhost:${port}`);
+})
